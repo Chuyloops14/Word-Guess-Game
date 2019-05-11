9bac4d7815a60a0025ba38288e10cdf4e4cb2ccc
chuyloops <alpizarjesus84@gmail.com>
2019-05-11 13:00:45 -0700

    Update game.js

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,77 +1,50 @@
-var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var won = 0;
+var lost = 0;
+var attempts = 10;
+var usedArray = [];
+var ranLetter = ranLetter;
+var letters = "qwertyuiopasdfghjklzxcvbnm"
+ 
+ranLetter = letters[Math.floor(Math.random() * letters.length)];
+console.log(ranLetter);
 
-let wins = 0;
-let losses = 0;
-let guesses = 9;
-let guessesLeft = 9;
-let guessedLetters = [];
-var letterToGuess = null;
+function jsGuess() {
+      ranLetter = letters[Math.floor(Math.random() * letters.length)];
+      console.log(ranLetter);
 
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-document.querySelector('.doc').style.display = "none";
-
-function updateGuessesLeft() {
-    document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
-};
-
-function updateLetterToGuess() {
-    this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
-};
-
-function updateGuessesSoFar() {
-    document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
-};
-var reset = function() {
-    totalGuesses = 9;
-    guessesLeft = 9;
-    guessedLetters = [];
-    setTimeout(function() { document.querySelector('.doc').style.display = 'none'; }, 5000);
-    updateLetterToGuess();
-    updateGuessesLeft();
-    updateGuessesSoFar();
 }
 
-updateLetterToGuess();
-updateGuessesLeft();
+document.onkeyup = function (event) {
+      var playerGuess = event.key;
+ 
+      if (playerGuess === ranLetter) {
+            won++;
+            attempts = 10;
+            usedArray = [];
 
-document.onkeyup = function(event) {
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    var check = computerChoices.includes(userGuess);
+      
+      jsGuess();
+      if (playerGuess !== ranLetter) {
+            attempts--;
 
-    if (check === false) {
-        ouch.play();
-        alert("That was not a valid guess, try again?");
-        return false;
-    } else if (check === true) {
-        guessesLeft--;
-        guessedLetters.push(userGuess);
-        updateGuessesLeft();
-        updateGuessesSoFar();
+      }
 
-        if (guessesLeft > 0) {
-            if (userGuess == letterToGuess) {
-                wins++;
-                yay.play();
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                userGuess = userGuess.toUpperCase();
-                document.querySelector('.doc').style.display = '';
-                document.querySelector('.doc').style.height = '4em';
-                document.querySelector('.doc').innerHTML = "Good guess Avenger " + userGuess + " was the letter I was thinking of!";
-                reset();
-            }
-        } else if (guessesLeft == 0) {
-            losses++;
-            boo.play();
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            document.querySelector('.doc').style.display = '';
-            document.querySelector('.doc').innerHTML = "Sorry Avenger , I was thinking of the letter " + letterToGuess;
+      if (attempts == 0) {
+            lost++;
+            usedArray = []
+            attempts = 10;
+      }
 
-            reset();
+      if (usedArray.indexOf(playerGuess) >= 0) {
 
-        }
-        return false;
-    } else {
-        alert("Oops, we have an error");
-    }
+      } else {
+            usedArray.push(playerGuess);
+            document.getElementById('playerGuess').innerHTML = usedArray;
+            console.log(usedArray);
 
-};
+      }
+      document.getElementById('won').innerHTML = won;
+      document.getElementById('lost').innerHTML = lost;
+      document.getElementById('attempts').innerHTML = attempts;
+
+}
